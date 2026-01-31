51315c8eaee01124c5db94f6bbf0ceb8c29360fd
claashk <claashk@gmx.de>
2026-01-31 19:32:09 +0100

    Added type annotations to payload.py

--- modbusclient/payload.py
+++ modbusclient/payload.py
@@ -1,8 +1,18 @@
-from datetime import datetime
 from .functions import *
+from .data_types import DataType
+
+from datetime import datetime, timezone
+
+try:
+    from typing import override
+except ImportError:
+    def override(f):
+        def inner(*args, **kwargs):
+            return f(*args, **kwargs)
+        return inner
 
 
-class Payload(object):
+class Payload:
     """Payload parser
 
     Small wrapper around parser objects to simplify payload definition. The
@@ -16,20 +26,32 @@ class Payload(object):
     For examples of compatible parser implementations refer to the `data_types`
     module.
 
-    Arguments:
-        dtype (object): Datatype object capable of converting from python object
+    Args:
+        dtype: Datatype object capable of converting from python object
             to bytes and back. Must provide methods ``encode`` and ``decode``.
-        address (int): Starting address (register number) of the message
-        mode (str): Permissible read write modes. Defaults to read only (``'r'``)
+        address: Starting address (register number) of the message
+        mode: Permissible read write modes. Defaults to read only (``'r'``)
+        reader: Read function as defined in module :mod:`~modbusclient.functions`
+            If ``None``, ``READ_HOLDING_REGISTERS`` is assumed if
+            ``self.is_writable`` or ``READ_INPUT_REGISTERS`` otherwise.
+        writer:
         **kwargs: Additional properties added verbatim to this instance.
     """
-    def __init__(self,
-                 dtype,
-                 address,
-                 mode='r',
-                 reader=None,
-                 writer=None,
-                 **kwargs):
+    _dtype: DataType
+    _address: int
+    _mode: str
+    _reader: int | None
+    _writer: int | None
+
+    def __init__(
+        self,
+        dtype: DataType,
+        address: int,
+        mode: str = 'r',
+        reader: int | None = None,
+        writer: int | None = None,
+        **kwargs: dict[str, object]
+    ) -> None:
         self._dtype = dtype
         self._address = address
         self._mode = mode
@@ -49,7 +71,7 @@ class Payload(object):
             else:
                 self._writer = WRITE_MULTIPLE_REGISTERS
 
-    def __len__(self):
+    def __len__(self) -> int:
         """Get length of this message in bytes
 
         Return:
@@ -57,8 +79,9 @@ class Payload(object):
         """
         return len(self._dtype)
 
-    def __str__(self):
-        components = ["Message {}".format(self.address)]
+    @override
+    def __str__(self) -> str:
+        components = [f"Message {self.address}"]
         for attr, fmt in [("name", " ({})"), ("units", " [{}]")]:
             try:
                 components.append(fmt.format(getattr(self, attr)))
@@ -66,7 +89,8 @@ class Payload(object):
                 pass
         return "".join(components)
 
-    def __hash__(self):
+    @override
+    def __hash__(self) -> int:
         """Get hash of this Message
 
         Return:
@@ -75,13 +99,14 @@ class Payload(object):
         """
         return hash(self._address)
 
-    def __eq__(self, other):
+    @override
+    def __eq__(self, other: "Payload") -> bool:
         """Equality comparison
 
         Two messages are considered equal, if and only if their addresses match.
 
-        Arguments:
-            other (:class:`~modbusclient.Payload`): Payload to compare to
+        Args:
+            other: Payload to compare to
 
         Return:
             bool: ``True`` if and only if the addresses of both messages are
@@ -89,11 +114,12 @@ class Payload(object):
         """
         return self._address == other._address
 
-    def __ne__(self, other):
+    @override
+    def __ne__(self, other: "Payload") -> bool:
         """Inequality comparison
 
-        Arguments:
-            other (:class:`~modbusclient.Payload`): Payload to compare to
+        Args:
+            other: Payload to compare to
 
         Return:
             bool: ``False`` if and only if the addresses of both messages are
@@ -102,82 +128,82 @@ class Payload(object):
         return self._address != other._address
 
     @property
-    def address(self):
+    def address(self) -> int:
         """Get address (numeric ID) of this message
 
         Return:
-            int: (starting) address of this message
+            (starting) address of this message
         """
         return self._address
 
     @property
-    def register_count(self):
+    def register_count(self) -> int:
         """Get number of registers used by this parser
 
         Return:
-            int: Number of 2-byte registers used by this parser
+            Number of 2-byte registers used by this parser
         """
         return (len(self) + 1) // 2
 
     @property
-    def reader(self):
+    def reader(self) -> int | None:
         """Get function code used to read this value from device
 
         Return:
-            int: Function code used to read this value from device. ``None`` if
+            Function code used to read this value from device. ``None`` if
             value cannot be read.
         """
         return self._reader
 
     @property
-    def writer(self):
+    def writer(self) -> int | None:
         """Get function code used to write this value to device
 
         Return:
-            int: Function code used to write this value to a device. ``None`` if
+            Function code used to write this value to a device. ``None`` if
             value cannot be written.
         """
         return self._writer
 
     @property
-    def is_readable(self):
+    def is_readable(self) -> bool:
         """Check if this message is readable"""
         return 'r' in self._mode
 
     @property
-    def is_writable(self):
+    def is_writable(self) -> bool:
         """Check if this message is writable"""
         return 'w' in self._mode
 
     @property
-    def is_write_protected(self):
-        """Check if message requires Grid guard code to be written
+    def is_write_protected(self) -> bool:
+        """Check if message requires some sort of login before write
 
         Return:
-            bool: True if and only if this message is writable with special
+            ``True`` if and only if this message is writable with special
             permissions only.
         """
         return "w!" in self._mode
 
-    def encode(self, value):
+    def encode(self, value: object) -> bytes:
         """Convert a value to binary modbus representation
 
-        Arguments:
-            value (object): Python variable to encode
+        Args:
+            value: Python variable to encode
 
         Return:
-            bytes: Packed binary version of :value:
+            Packed binary version of ``value``.
         """
         return self._dtype.encode(value)
 
-    def decode(self, buffer):
+    def decode(self, buffer: bytes) -> object:
         """Convert bytes from binary modbus representation into variable
 
-        Arguments:
-            buffer (bytes): Data in packed binary format
+        Args:
+            buffer: Data in packed binary format
 
         Return:
-            object: Python representation of the data
+            Python representation of the data
         """
         return self._dtype.decode(buffer)
 
@@ -185,45 +211,68 @@ class Payload(object):
 class Enum(Payload):
     """Specialisation for enumeration messages
 
-    Accepts an additional dictionary as argument, which relates the keys to
-    a string describing the meaning.
+    Accepts an additional dictionary as argument, which maps payload values
+    to strings.
 
-    Arguments:
-        dtype (object): Datatype object capable of converting from python object
+    Args:
+        dtype: Datatype object capable of converting from python object
             to bytes and back. Must provide methods ``encode`` and ``decode``.
-        address (int): Starting address (register number) of the message
-        choices(dict): Dictionary mapping enum values to values
-        mode (str): Permissible read write modes. Defaults to read only (``'r'``)
+        address: Starting address (register number) of the message
+        choices: Dictionary mapping strings to values
+        mode: Permissible read write modes. Defaults to read only (``'r'``)
         **kwargs: Additional properties added verbatim to this instance.
     """
-    def __init__(self, dtype, address, choices=dict(), mode="r", **kwargs):
-        super().__init__(dtype, address=address, mode=mode, **kwargs)
-        self.choices = choices
+    choices: dict[str, object]
 
-    def __getitem__(self, name):
+    def __init__(
+        self,
+        dtype: DataType,
+        address: int,
+        choices: dict[str, object] | None= None,
+        mode: str = "r",
+        **kwargs: dict[str, object]
+    ) -> None:
+        super().__init__(dtype, address=address, mode=mode, **kwargs)
+        if choices is None:
+            self.choices = dict()
+        else:
+            self.choices= dict(choices)
+
+    def __getitem__(self, name: str) -> object:
         return self.choices[name]
 
 
 class Fixpoint(Payload):
     """Specialisation of :class:´Payload` for fixed point floats
 
-    Arguments:
-        dtype (object): Datatype object capable of converting from python object
+    Args:
+        dtype: Datatype object capable of converting from python object
             to bytes and back. Must provide methods ``encode`` and ``decode``.
         address (int): Starting address (register number) of the message
         mode (str): Permissible read write modes. Defaults to read only (``'r'``)
         digits (int): Number of decimal digits. Defaults to 0.
         **kwargs: Additional properties added verbatim to this instance.
     """
-    def __init__(self, dtype, address, mode="r", digits=0, **kwargs):
+    _scale: int
+
+    def __init__(
+        self,
+        dtype: DataType,
+        address: int,
+        mode: str = "r",
+        digits: int = 0,
+        **kwargs: dict[str, object]
+    ) -> None:
         super().__init__(dtype, address=address, mode=mode, **kwargs)
         self._scale = 10**digits
 
-    def encode(self, value):
+    @override
+    def encode(self, value: float) -> bytes:
         return super().encode(int(self._scale * value + 0.5))
 
-    def decode(self, bytes):
-        val = super().decode(bytes)
+    @override
+    def decode(self, buffer: bytes) -> float | None:
+        val = super().decode(buffer)
         if val is None:
             return None
         return float(val) / self._scale
@@ -231,11 +280,11 @@ class Fixpoint(Payload):
 
 class Timestamp(Payload):
     """Message for times in seconds since 1970-01-01"""
-    def encode(self, utc):
-        super.encode(int(utc.timestamp() + 0.5))
+    def encode(self, utc: datetime) -> bytes:
+        return super().encode(int(utc.timestamp()))
 
-    def decode(self, buffer):
+    def decode(self, buffer: bytes) -> datetime | None:
         sec = super().decode(buffer)
         if sec is None:
             return None
-        return datetime.utcfromtimestamp(sec)
+        return datetime.fromtimestamp(sec, timezone.utc)
