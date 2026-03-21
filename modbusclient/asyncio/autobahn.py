e2719c585709e64196c7e1f701abdba75fac31bb
claashk <claashk@gmx.de>
2026-03-21 15:02:12 +0100

    Fixed annotations in autobahn.py

--- modbusclient/asyncio/autobahn.py
+++ modbusclient/asyncio/autobahn.py
@@ -1,109 +1,117 @@
 from autobahn.asyncio.component import Component
+from autobahn.wamp.protocol import ApplicationSession
+from .api_wrapper import ApiWrapper
 
-
-class ComponentBase(object):
+class ComponentBase:
     """A base class for WAMP components
 
     This class provides a template to turn an asynchronous Modbus client into a
     WAMP client using the Autobahn package. Provided you have an API client
     derived from  :class:`modbusclient.asyncio.ApiWrapper`, you can use this
-    class as base class for your WAMP client and provice the API client
+    class as base class for your WAMP client and provide the API client
     as `client` argument. This avoids the repetition of some boiler plate code.
 
-    Arguments:
-        transports (list or str): Passed verbatim to
+    Args:
+        transports: Passed verbatim to
             :class:`autobahn.asyncio.component.Component` as `transports`.
-        realm (str): WAMP realm. Passed verbatim to
+        realm: WAMP realm. Passed verbatim to
             :class:`autobahn.asyncio.component.Component` as `realm`.
-        client (:class:`modbusclient.asyncio.ApiWrapper`): An implementation of
-            an API wrapper.
-
-    Attributes:
-        component (:class:`autobahn.asyncio.component.Component`): Autobahn
-            WAMP component wrapped by this class
-        session (:class:`autobahn.wamp.protocol.ApplicationSession`): Session
-            passed during on_join. ``None`` if client has not joined any session.
+        client: API wrapper implementation.
     """
-    def __init__(self, transports, realm, client):
-        self._component = Component(transports=transports, realm=realm)
-        self._client = client
+    def __init__(
+        self,
+        transports: list[str] | str,
+        realm: str,
+        client: ApiWrapper
+    ) -> None:
+        self._component: Component = Component(transports=transports, realm=realm)
+        self._client: ApiWrapper = client
         self._component.on('join', self._join)
         self._component.on('leave', self._leave)
 
-        self._session = None
+        self._session: ApplicationSession | None = None
 
     @property
-    def component(self):
-        """Get component wrapped by this instance
+    def component(self) -> Component:
+        """Access component wrapped by this instance
 
         Return:
-            :class:`autobahn.asyncio.component.Component`: Autobahn
-            WAMP component wrapped by this class
+            Autobahn WAMP component wrapped by this class
         """
         return self._component
 
     @property
-    def session(self):
-        """Get currently joined session
+    def session(self)-> ApplicationSession | None:
+        """Access currently joined session
 
         Return:
-            :class:`autobahn.wamp.protocol.ApplicationSession`: Session passed
-                during on_join. ``None`` if client is currently not joined to
-                any session.
+            Session passed during ``on_join``. ``None`` if client is currently
+            not joined to any session.
         """
         return self._session
 
-    async def _join(self, session, details):
+    async def _join(
+        self,
+        session: ApplicationSession,
+        details: dict[str, str]
+    ) -> None:
         """Call back invoked when joining (on_join)
 
         Sets the internal session member variable and prints an info message.
 
-        Arguments:
-            session (:class:`autobahn.wamp.protocol.ApplicationSession`):
-                Application session.
-            details (dict): Dictionary with details.
+        Args:
+            session: Application session.
+            details: Dictionary with details.
         """
         self._session = session
-        self.info('Joined session {session}: {details}',
-                  session=session, details=details)
+        self.info(f'Joined session {session}: {details}')
 
-    async def _leave(self, session, reason):
-        self.info("Disconnecting from session {session}. Reason: {reason}",
-                  session=session, reason=reason)
+    async def _leave(
+        self,
+        session: ApplicationSession,
+        reason: dict[str, str]
+    ) -> None:
+        """Call back invoked when leaving (on_leave)
+
+        Args:
+            session: Application session.
+            details: Dictionary with details.
+        """
+        self.info(f"Disconnecting from session {session}. Reason: {reason}")
         self._session = None
 
-    def debug(self, msg, **kwargs):
+    def debug(self, msg: str, **kwargs) -> None:
         """Create debug level log message
 
-        Arguments:
-            msg (str): Log message.
+        Args:
+            msg: Log message.
             **kwargs: Keyword arguments passed to message formatter
         """
         self._component.log.debug(msg, **kwargs)
 
-    def info(self, msg, **kwargs):
+    def info(self, msg: str, **kwargs) -> None:
         """Create info level log message
 
-        Arguments:
-            msg (str): Log message.
+        Args:
+            msg: Log message.
             **kwargs: Keyword arguments passed to message formatter
         """
         self._component.log.info(msg, **kwargs)
 
-    def warning(self, msg, **kwargs):
+    def warning(self, msg: str, **kwargs) -> None:
         """Create warning level log message
 
-        Arguments:
-            msg (str): Log message.
+        Args:
+            msg: Log message.
             **kwargs: Keyword arguments passed to message formatter
         """
         self._component.log.warning(msg, **kwargs)
 
-    def error(self, msg, **kwargs):
+    def error(self, msg: str, **kwargs) -> None:
         """Create error level log message
 
-        Arguments:
-            msg (str): Log message.
+        Args:
+            msg: Log message.
             **kwargs: Keyword arguments passed to message formatter
         """
         self._component.log.error(msg, **kwargs)
